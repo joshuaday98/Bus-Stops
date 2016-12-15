56242c47ab8555db5b033e19bc1ce1e9c6608166
Joshua <joshua.f.day@gmail.com>
2016-12-15 13:37:04 -0800

    Landing menu works needs creation/login functionality

--- bus_stop/bus_stops/static/js/landing.js
+++ bus_stop/bus_stops/static/js/landing.js
@@ -2,18 +2,32 @@
 
 
 function login_form(){
-  
-}
+  $('#land').fadeOut(function(){
+    $('#loginform').fadeIn();
+  });
+};
 
 
 function guest_form(){
+  $('#land').fadeOut(function(){
+    $('#guest').fadeIn();
+  });
+};
 
-}
+
+function create_form(){
+  $('#land').fadeOut(function(){
+    $('#createacc').fadeIn();
+  });
+};
 
 
 (function direction(){
   $('#login').on('click touchstart', function(evt){
     login_form()
+  });
+  $('#create').on('click touchstart', function(evt){
+    create_form()
   });
   $('#continue').on('click touchstart', function(evt){
     guest_form()
